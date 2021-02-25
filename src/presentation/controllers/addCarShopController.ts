5c5821f2acdf04c5308c0a3059475f6fd2fbab16
Benjamim <benjamimq015@gmail.com>
2021-02-25 12:10:39 -0400

    feat: ensure AddCarShopController returns 400 if an invalid cnpj is provided

--- src/presentation/controllers/addCarShopController.ts
+++ src/presentation/controllers/addCarShopController.ts
@@ -2,14 +2,22 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missingParamError';
 import { badRequest } from '../helpers/http';
 import { Controller } from '../protocols/controller';
+import { CnpjValidator } from '../protocols/cnpjValidator';
+import { InvalidParamError } from '../errors/invalidParamError';
 
 export class AddCarShopController implements Controller {
+  constructor(private cnpjValidator: CnpjValidator) {}
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'cnpj'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+    const isValidCnpj = this.cnpjValidator.isValid(httpRequest.body.cnpj);
+    if (!isValidCnpj) {
+      return badRequest(new InvalidParamError('cnpj'));
     }
   }
 }
