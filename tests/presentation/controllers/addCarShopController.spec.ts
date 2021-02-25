8b324f58f50a83ea3aa94caf72d81f4d97402c5d
Benjamim <benjamimq015@gmail.com>
2021-02-24 22:37:21 -0400

    test: ensure AddCarShopController returns 400 if no cnpj is provided

--- tests/presentation/controllers/addCarShopController.spec.ts
+++ tests/presentation/controllers/addCarShopController.spec.ts
@@ -4,10 +4,24 @@ describe('AddCarShopController', () => {
   it('Should return 400 if no name is provided', () => {
     const sut = new AddCarShopController();
     const httpRequest = {
-      cnpj: 'any_cnpj',
+      body: {
+        cnpj: 'any_cnpj',
+      },
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+  });
+
+  it('Should return 400 if no cnpj is provided', () => {
+    const sut = new AddCarShopController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: cnpj'));
   });
 });
