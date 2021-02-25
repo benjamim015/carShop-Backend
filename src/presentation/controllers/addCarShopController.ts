d0998f1fd8db538d5616947875620677536aeced
Benjamim <benjamimq015@gmail.com>
2021-02-24 22:54:51 -0400

    refactor: make addCarShopController conforms to a protocol

--- src/presentation/controllers/addCarShopController.ts
+++ src/presentation/controllers/addCarShopController.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missingParamError';
 import { badRequest } from '../helpers/http';
+import { Controller } from '../protocols/controller';
 
-export class AddCarShopController {
+export class AddCarShopController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'cnpj'];
     for (const field of requiredFields) {
