f28c722d027a76f8f5f97163e8267e3eb2249af8
Benjamim <benjamimq015@gmail.com>
2021-02-25 12:10:45 -0400

    test: ensure AddCarShopController returns 400 if an invalid cnpj is provided

--- tests/presentation/controllers/addCarShopController.spec.ts
+++ tests/presentation/controllers/addCarShopController.spec.ts
@@ -1,11 +1,30 @@
 import { AddCarShopController } from '@/presentation/controllers/addCarShopController';
 import { MissingParamError } from '@/presentation/errors/missingParamError';
+import { InvalidParamError } from '@/presentation/errors/invalidParamError';
+import { CnpjValidator } from '@/presentation/protocols/cnpjValidator';
 
-const makeSut = (): AddCarShopController => new AddCarShopController();
+type SutTypes = {
+  sut: AddCarShopController;
+  cnpjValidatorStub: CnpjValidator;
+};
+
+const makeSut = (): SutTypes => {
+  class CnpjValidatorStub implements CnpjValidator {
+    isValid(cnpj: string): boolean {
+      return true;
+    }
+  }
+  const cnpjValidatorStub = new CnpjValidatorStub();
+  const sut = new AddCarShopController(cnpjValidatorStub);
+  return {
+    sut,
+    cnpjValidatorStub,
+  };
+};
 
 describe('AddCarShopController', () => {
   it('Should return 400 if no name is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         cnpj: 'any_cnpj',
@@ -17,7 +36,7 @@ describe('AddCarShopController', () => {
   });
 
   it('Should return 400 if no cnpj is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -26,5 +45,19 @@ describe('AddCarShopController', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('cnpj'));
+  });
+
+  it('Should return 400 if an invalid cnpj is provided', () => {
+    const { sut, cnpjValidatorStub } = makeSut();
+    jest.spyOn(cnpjValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        cnpj: 'invalid_cnpj',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('cnpj'));
   });
 });
