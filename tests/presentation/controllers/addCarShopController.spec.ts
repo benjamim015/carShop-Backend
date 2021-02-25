aa17c4019c8b1b8748fe5f0414f8b2fce08c3b64
Benjamim <benjamimq015@gmail.com>
2021-02-24 22:32:57 -0400

    test: ensure AddCarShopController returns an error if no name is provided

--- tests/presentation/controllers/addCarShopController.spec.ts
+++ tests/presentation/controllers/addCarShopController.spec.ts
@@ -8,5 +8,6 @@ describe('AddCarShopController', () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
 });
