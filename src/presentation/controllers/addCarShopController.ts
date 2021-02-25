022fb893b12ac89579f7780f5029fe502ae32886
Benjamim <benjamimq015@gmail.com>
2021-02-25 12:42:33 -0400

    feat: ensure AddCarShopController returns 500 if CnpjValidator throws

--- src/presentation/controllers/addCarShopController.ts
+++ src/presentation/controllers/addCarShopController.ts
@@ -4,20 +4,28 @@ import { badRequest } from '../helpers/http';
 import { Controller } from '../protocols/controller';
 import { CnpjValidator } from '../protocols/cnpjValidator';
 import { InvalidParamError } from '../errors/invalidParamError';
+import { ServerError } from '../errors/serverError';
 
 export class AddCarShopController implements Controller {
   constructor(private cnpjValidator: CnpjValidator) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'cnpj'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['name', 'cnpj'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
-    const isValidCnpj = this.cnpjValidator.isValid(httpRequest.body.cnpj);
-    if (!isValidCnpj) {
-      return badRequest(new InvalidParamError('cnpj'));
+      const isValidCnpj = this.cnpjValidator.isValid(httpRequest.body.cnpj);
+      if (!isValidCnpj) {
+        return badRequest(new InvalidParamError('cnpj'));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
