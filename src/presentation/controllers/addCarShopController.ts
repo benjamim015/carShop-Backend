54bae2edf77caaef34bb418fdf8485568d574cb1
Benjamim <benjamimq015@gmail.com>
2021-02-24 22:47:59 -0400

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/addCarShopController.ts
+++ src/presentation/controllers/addCarShopController.ts
@@ -1,19 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missingParamError';
+import { badRequest } from '../helpers/http';
 
 export class AddCarShopController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name'),
-      };
+      return badRequest(new MissingParamError('name'));
     }
     if (!httpRequest.body.cnpj) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('cnpj'),
-      };
+      return badRequest(new MissingParamError('cnpj'));
     }
   }
 }
