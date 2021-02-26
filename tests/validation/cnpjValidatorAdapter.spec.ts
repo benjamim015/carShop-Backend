15bb898dad426a6ec42a5f4243db291cf5e856f5
Benjamim <benjamimq015@gmail.com>
2021-02-25 23:16:22 -0400

    test: ensure CnpjValidatorAdapter returns true if validator returns true

--- tests/validation/cnpjValidatorAdapter.spec.ts
+++ tests/validation/cnpjValidatorAdapter.spec.ts
@@ -1,11 +1,27 @@
 import { CnpjValidatorAdapter } from '@/validation/cnpjValidator';
+import { cnpjValidator } from 'some-validations';
 
 const makeSut = (): CnpjValidatorAdapter => new CnpjValidatorAdapter();
+
+jest.mock('some-validations', () => ({
+  cnpjValidator: {
+    isValid(): boolean {
+      return true;
+    },
+  },
+}));
 
 describe('CnpjValidator Adapter', () => {
   it('should return false if validator returns false', () => {
     const sut = makeSut();
+    jest.spyOn(cnpjValidator, 'isValid').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_cnpj');
     expect(isValid).toBe(false);
+  });
+
+  it('should return true if validator returns true', () => {
+    const sut = makeSut();
+    const isValid = sut.isValid('valid_cnpj');
+    expect(isValid).toBe(true);
   });
 });
