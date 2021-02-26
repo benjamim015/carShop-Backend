611614c92b33fd727797f90c073b914153b4aa23
Benjamim <benjamimq015@gmail.com>
2021-02-26 19:53:41 -0400

    feat: ensure DbAddCarShop returns an car shop on success

--- src/data/useCases/addCarShop/dbAddCarShop.ts
+++ src/data/useCases/addCarShop/dbAddCarShop.ts
@@ -9,7 +9,7 @@ export class DbAddCarShop implements AddCarShop {
   constructor(private addCarShopRepository: AddCarShopRepository) {}
 
   async add(carShopData: AddCarShopModel): Promise<CarShopModel> {
-    await this.addCarShopRepository.add(carShopData);
-    return null;
+    const carShop = await this.addCarShopRepository.add(carShopData);
+    return carShop;
   }
 }
