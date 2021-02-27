673eb392b289d01004e0e01535d79083981b0917
Benjamim <benjamimq015@gmail.com>
2021-02-27 18:05:20 -0400

    feat: add AddCarShopController factory

---
+++ src/main/factories/addCarShop.ts
@@ -0,0 +1,11 @@
+import { AddCarShopController } from '@/presentation/controllers/addCarShop/addCarShopController';
+import { CnpjValidatorAdapter } from '@/validation/cnpjValidatorAdapter';
+import { DbAddCarShop } from '@/data/useCases/addCarShop/dbAddCarShop';
+import { CarShopPgTypeORMRepository } from '@/infra/db/postgres/orm/typeorm/repositories/carShopRepository';
+
+export const makeAddCarShopController = (): AddCarShopController => {
+  const cnpjValidatorAdapter = new CnpjValidatorAdapter();
+  const carShopPgTypeORMRepository = new CarShopPgTypeORMRepository();
+  const dbAddCarShop = new DbAddCarShop(carShopPgTypeORMRepository);
+  return new AddCarShopController(cnpjValidatorAdapter, dbAddCarShop);
+};
