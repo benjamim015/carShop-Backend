ba878c7e808ffcb443f22395c8378c7ba80727e0
Benjamim <benjamimq015@gmail.com>
2021-02-28 17:43:52 -0400

    test: ensure CarShopPgTypeORMRepository load all car shops

--- tests/infra/db/postgres/orm/typeorm/repositories/carShopRepository.spec.ts
+++ tests/infra/db/postgres/orm/typeorm/repositories/carShopRepository.spec.ts
@@ -1,5 +1,20 @@
 import { CarShopPgTypeORMRepository } from '@/infra/db/postgres/orm/typeorm/repositories/carShopRepository';
 import { TypeORMHelper } from '@/infra/db/postgres/orm/typeorm/helper';
+import { CarShop } from '@/infra/db/postgres/orm/typeorm/entities/carShop';
+
+const makeFakeCarShops = async () => {
+  const carShopsRepository = TypeORMHelper.instance.getRepository(CarShop);
+  const carShop1 = carShopsRepository.create({
+    name: 'any_name',
+    cnpj: 'any_cnpj',
+  });
+  await carShopsRepository.save(carShop1);
+  const carShop2 = carShopsRepository.create({
+    name: 'other_name',
+    cnpj: 'other_cnpj',
+  });
+  await carShopsRepository.save(carShop2);
+};
 
 interface SutTypes {
   sut: CarShopPgTypeORMRepository;
@@ -12,7 +27,7 @@ const makeSut = (): SutTypes => {
   };
 };
 
-describe('CarShop TypeORM Repository', () => {
+describe('CarShop TypeORM Postgres Repository', () => {
   beforeAll(async () => {
     await TypeORMHelper.instance.connect();
   });
@@ -38,6 +53,17 @@ describe('CarShop TypeORM Repository', () => {
       expect(carShop).toHaveProperty('cnpj', 'any_cnpj');
       expect(carShop).toHaveProperty('created_at');
       expect(carShop).toHaveProperty('updated_at');
+    });
+  });
+
+  describe('loadAll()', () => {
+    it('should load all car shops on success', async () => {
+      const { sut } = makeSut();
+      await makeFakeCarShops();
+      const carShops = await sut.loadAll();
+      expect(carShops.length).toBe(2);
+      expect(carShops[0].name).toBe('any_name');
+      expect(carShops[1].name).toBe('other_name');
     });
   });
 });
