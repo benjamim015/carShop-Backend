4578c340ecfaed893fbe0de59f20d40b225961dd
Benjamim <benjamimq015@gmail.com>
2021-02-28 13:11:32 -0400

    test: ensure ValidationComposite returns the first error if more then one validation fails

--- tests/presentation/helpers/validators/validationComposite.spec.ts
+++ tests/presentation/helpers/validators/validationComposite.spec.ts
@@ -2,30 +2,45 @@ import { MissingParamError } from '@/presentation/errors';
 import { Validation } from '@/presentation/helpers/validation/validation';
 import { ValidationComposite } from '@/presentation/helpers/validation/validationComposite';
 
-class ValidationStub implements Validation {
-  validate(input: any): Error {
-    return new MissingParamError('field');
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(_input: any): Error {
+      return null;
+    }
   }
-}
-
+  return new ValidationStub();
+};
 type SutType = {
   sut: ValidationComposite;
-  validationStub: ValidationStub;
+  validationStubs: Validation[];
 };
 
 const makeSut = (): SutType => {
-  const validationStub = new ValidationStub();
-  const sut = new ValidationComposite([validationStub]);
+  const validationStubs = [makeValidation(), makeValidation()];
+  const sut = new ValidationComposite(validationStubs);
   return {
     sut,
-    validationStub,
+    validationStubs,
   };
 };
 
 describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
-    const { sut } = makeSut();
+    const { sut, validationStubs } = makeSut();
+    jest
+      .spyOn(validationStubs[1], 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
+  });
+
+  it('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut();
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error());
+    jest
+      .spyOn(validationStubs[1], 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
+    const error = sut.validate({ field: 'any_value' });
+    expect(error).toEqual(new Error());
   });
 });
