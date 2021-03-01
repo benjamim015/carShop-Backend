bca8e8e8dd7af013f7de1ffe6672f7438d5d5a9c
Benjamim <benjamimq015@gmail.com>
2021-03-01 11:45:16 -0400

    feat: add CreateCars migration

---
+++ src/infra/db/postgres/orm/typeorm/migrations/1614612725604-CreateCars.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCars1614612725604 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+          },
+          {
+            name: 'model',
+            type: 'varchar',
+          },
+          {
+            name: 'year',
+            type: 'integer',
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'money',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars');
+  }
+}
