72ceaff020e454db996871ae0de937710f70c0bd
Benjamim <benjamimq015@gmail.com>
2021-03-02 19:02:06 -0400

    feat: serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import setupStaticFiles from './staticFiles';
 import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
 
 const app = express();
+setupStaticFiles(app);
 setupMiddlewares(app);
 setupRoutes(app);
 export default app;
